5feff1e38386bad031c16a23c58685f1ca5f5607
momotadhali <mamotadhali1998@gmail.com>
2024-09-03 12:11:38 +0600

    first commit

---
+++ javascript.js
@@ -0,0 +1,31 @@
+// var x=70;
+// var y=20;
+// var z=x+y;
+// console.log(z);
+
+// function myFunction(){
+//    document.getElementById("demo").innerHTML='hi phd' ;
+//    document.getElementById("Demo").style.fontSize = "35px";
+// }
+
+// function myFunction(name){
+//     console.log(" hi" + name);
+// }
+// myFunction(" mitu");
+// myFunction(" rikta");
+// myFunction(" sumi");
+// myFunction(" mamota");
+
+// function myFunction(){
+// document.getElementById('myImage').src='image/WhatsApp Image 2024-08-18 at 17.25.01_81cd16bb.jpg';
+// document.getElementById('myImage').src='image/WhatsApp Image 2024-09-01 at 15.39.41_ece483e1.jpg';
+// }
+
+// function myFunction(){
+
+//     document.getElementById('class').style.display="none";
+// }
+
+    function myFunction(){
+   document.getElementById('Mamota').style.display='block';
+    }
